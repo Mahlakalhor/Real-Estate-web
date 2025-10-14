ff0364e7be5d5708c7f2e7acf2c90211b0beef27
Mahla <Mahlakalhor82@gmail.com>
2025-10-14 02:48:44 -0700

    v1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,44 @@
-import './style.css'
+import "./style.css";
+import "./style.css";
 
+const cardsEl = document.querySelectorAll(".card");
+const housesEl = document.getElementById("houses");
+
+const loadHouses = async () => {
+  const res = await fetch(
+    "https://675ec6a21f7ad24269969bf9.mockapi.io/real-state/real-state"
+  );
+  const houses = await res.json();
+  console.log(houses);
+
+  housesEl.innerHTML = "";
+
+  houses.forEach((house) => {
+    housesEl.innerHTML += `
+      <div class="flex flex-col w-[300px] rounded-md gap-4 transition-all hover:scale-110 hover:bg-[#e8e5f8] p-6">
+        <div class="w-[250px]">
+          <img class="w-full object-cover rounded-lg" src="${house.image}" alt="home-pic" />
+        </div>
+        <span class="text-xl text-[#8c8b8b]">
+          <span class="text-[#ff922d]">$</span>${house.price}
+        </span>
+        <span class="text-[#1f3e72] text-2xl font-bold">${house.name}</span>
+        <p class="text-[#8c8b8b] text-xs">${house.adress}</p>
+      </div>
+    `;
+  });
+};
+
+loadHouses();
+
+cardsEl.forEach((card) => {
+  const paragraph = card.querySelector("p");
+
+  card.addEventListener("click", () => {
+    if (paragraph.classList.contains("hidden")) {
+      paragraph.classList.remove("hidden");
+    } else {
+      paragraph.classList.add("hidden");
+    }
+  });
+});
