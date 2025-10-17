b8270f8e4e96f387b82c8618b389e019de98b630
Mahla <Mahlakalhor82@gmail.com>
2025-10-17 15:22:55 -0700

    v1

--- src/main.js
+++ src/main.js
@@ -9,15 +9,18 @@ const loadHouses = async () => {
     "https://675ec6a21f7ad24269969bf9.mockapi.io/real-state/real-state"
   );
   const houses = await res.json();
+  const files = ["state1.png", "state2.png", "state3.png"];
+
   console.log(houses);
 
   housesEl.innerHTML = "";
 
-  houses.forEach((house) => {
+  houses.forEach((house, i) => {
+    const imgFile = files[i % files.length];
     housesEl.innerHTML += `
       <div class="flex flex-col w-[300px] rounded-md gap-4 transition-all hover:scale-110 hover:bg-[#e8e5f8] p-6">
         <div class="w-[250px]">
-          <img class="w-full object-cover rounded-lg" src="/${house.image}" alt="home-pic" />
+          <img class="w-full object-cover rounded-lg" src="./src/image/${imgFile}" alt="home-pic" />
         </div>
         <span class="text-xl text-[#8c8b8b]">
           <span class="text-[#ff922d]">$</span>${house.price}
